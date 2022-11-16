7ef0180ce060bb527eee5a78e6dc26a9c18c1acb
Oguz Koran <Ouz_x3@hotmail.com>
2022-11-16 20:13:18 +0300

    Add project files.

---
+++ 01.AriBilgi.Blog.Shared/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.AriBilgi.Blog.Shared
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeteledDate { get; set; }
+        public string? DeletedBy { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public string? ModifiedBy { get; set; }
+
+    }
+}
---
+++ 03.AriBilgi.Blog.Data/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using _01.AriBilgi.Blog.Shared;
+using _025.AriBilgi.Blog.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.AriBilgi.Blog.Data.Repositories
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
